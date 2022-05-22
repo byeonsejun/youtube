79c65b9444dd893a9b36fcfd129ed8eeedd26c0c
byeonsejun <qustpwns93@gmail.com>
2022-05-22 17:00:21 +0900

    youtube 0522

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app';
+import Youtube from './service/youtube';
 
-const rootNode = document.getElementById('root');
+const rootNode = document.getElementById('root')
 
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY)
 ReactDOM.createRoot(rootNode).render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
 );
